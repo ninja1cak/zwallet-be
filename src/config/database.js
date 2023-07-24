83a37db5c1d8bf9c062f366f95e4ac6f1ecfad1c
hauzankhoirullah <hauzan41200@gmail.com>
2023-07-24 19:13:51 +0700

    add vercel config

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,7 @@ const {Pool} = require('pg')
 require('dotenv/config')
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD
-})
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
 
 module.exports = pool
